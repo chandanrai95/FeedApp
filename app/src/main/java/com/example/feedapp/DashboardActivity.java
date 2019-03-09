f0f043bdf1913f7b78f387f592415664b7da7b68
chandanrai95 <chandan.rai1995@gmail.com>
2019-03-09 17:15:15 +0530

    Add feedback firebase

--- app/src/main/java/com/example/feedapp/DashboardActivity.java
+++ app/src/main/java/com/example/feedapp/DashboardActivity.java
@@ -1,5 +1,6 @@
 package com.example.feedapp;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -23,12 +25,25 @@ public class DashboardActivity extends AppCompatActivity {
     private Toolbar mToolbar;
     private FloatingActionButton mCreatePost;
     private DatabaseReference mDatabaseRefer;
+    private String Name,Email;private
+    ProgressDialog mprogress;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
         init();
+
+        mCreatePost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(DashboardActivity.this,PostCreateActivity.class);
+                intent.putExtra("name",Name);
+                intent.putExtra("email",Email);
+                startActivity(intent);
+            }
+        });
     }
 
     public void init()
@@ -42,6 +57,8 @@ public class DashboardActivity extends AppCompatActivity {
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_person_black_24dp);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        mprogress=new ProgressDialog(DashboardActivity.this);
         mDatabaseRefer= FirebaseDatabase.getInstance().getReference().child("Users").child(currentUser.getUid());
 
         getuserData();
@@ -50,17 +67,26 @@ public class DashboardActivity extends AppCompatActivity {
 
     public void getuserData()
     {
+        mprogress.show();
+        mprogress.setTitle("Fetching");
+        mprogress.setMessage("Please wait while we Fetching Details");
+        mprogress.setCanceledOnTouchOutside(false);
+
         mDatabaseRefer.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+                mprogress.dismiss();
                 getSupportActionBar().setTitle(dataSnapshot.child("Name").getValue().toString());
+                Name=dataSnapshot.child("Name").getValue().toString();
+                Email=dataSnapshot.child("Email").getValue().toString();
+
                 mToolbar.setTitle(dataSnapshot.child("Name").getValue().toString());
                 mToolbar.setTitleTextColor(getApplicationContext().getResources().getColor(R.color.white));
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
+                mprogress.dismiss();
             }
         });
     }
--- app/src/main/java/com/example/feedapp/PostCreateActivity.java
+++ app/src/main/java/com/example/feedapp/PostCreateActivity.java
@@ -1,13 +1,25 @@
 package com.example.feedapp;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class PostCreateActivity extends AppCompatActivity {
 
@@ -16,15 +28,43 @@ public class PostCreateActivity extends AppCompatActivity {
     private TextInputLayout mFeedback;
     private Button mPost;
     private DatabaseReference mDataBaseReference;
+    private FirebaseUser currentUser;
+    private String name,email;
+    ProgressDialog mprogress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_create);
         init();
+
+        mPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String feed=mFeedback.getEditText().getText().toString();
+                if (!TextUtils.isEmpty(feed) && !TextUtils.isEmpty(name) && !TextUtils.isEmpty(email))
+                {
+                    mprogress.show();
+                    mprogress.setTitle("Adding Feedback");
+                    mprogress.setMessage("Please wait while we Adding Your Feedback");
+                    mprogress.setCanceledOnTouchOutside(false);
+
+                    createPost(feed,name,email);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"Please Fill Details Properly",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
 
     public void init()
     {
+        Intent intent=getIntent();
+
+        name=intent.getStringExtra("name");
+        email=intent.getStringExtra("email");
+
         mFireAuth=FirebaseAuth.getInstance();
         mToolbar=(Toolbar) findViewById(R.id.post_toolbar);
         mFeedback=(TextInputLayout) findViewById(R.id.textInputLayout_feedback);
@@ -32,11 +72,30 @@ public class PostCreateActivity extends AppCompatActivity {
 
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setTitle("Create Post");
+        getSupportActionBar().setTitle("Create Feedback");
+
+        mprogress=new ProgressDialog(PostCreateActivity.this);
     }
 
     private void createPost(String Feed,String name, String email)
     {
-
+        currentUser=mFireAuth.getCurrentUser();
+        mDataBaseReference= FirebaseDatabase.getInstance().getReference().child("Post").child(currentUser.getUid());
+        HashMap<String,String> dataMap=new HashMap<>();
+        dataMap.put("Feed",Feed);
+        dataMap.put("Created By",name+","+email);
+        mDataBaseReference.setValue(dataMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                mprogress.dismiss();
+                if (task.isSuccessful())
+                {
+                    Toast.makeText(getApplicationContext(),"Feedback Added Successfully",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"Error Occured",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
