244a2b660d5297f43b971b36cbb3301ededdf49a
chandanrai95 <chandan.rai1995@gmail.com>
2019-03-09 15:03:10 +0530

    create user login

---
+++ app/src/main/java/com/example/feedapp/CreateLoginActivity.java
@@ -0,0 +1,84 @@
+package com.example.feedapp;
+
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+
+public class CreateLoginActivity extends AppCompatActivity {
+
+    private TextInputLayout mDisplayName,mEmail,mPassword;
+    private Button mCreate;
+    private FirebaseAuth mAuth=FirebaseAuth.getInstance();
+    private Toolbar mtoolbar;
+    private DatabaseReference mdatabaseRefer;
+    private ProgressDialog mprogress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_login);
+        init();
+        mCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name=mDisplayName.getEditText().getText().toString();
+                String email=mEmail.getEditText().getText().toString();
+                String pass=mPassword.getEditText().getText().toString();
+                if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(pass))
+                {
+                    createUser(name,email,pass);
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(),"Please Fill Details Properly",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+
+    public void init()
+    {
+        mDisplayName=(TextInputLayout) findViewById(R.id.name);
+        mEmail=(TextInputLayout) findViewById(R.id.email);
+        mPassword=(TextInputLayout) findViewById(R.id.password);
+        mCreate=(Button) findViewById(R.id.create_id);
+        mtoolbar=(Toolbar)findViewById(R.id.register_toolbar);
+
+        setSupportActionBar(mtoolbar);
+        getSupportActionBar().setTitle("Create Account");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mprogress=new ProgressDialog(CreateLoginActivity.this);
+    }
+
+    public void createUser(String Name, String Email, String Pass)
+    {
+        mAuth.createUserWithEmailAndPassword(Email,Pass)
+        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {
+                    Toast.makeText(getApplicationContext(),"Registered Succesfully",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"Error Occured",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/feedapp/MainActivity.java
+++ app/src/main/java/com/example/feedapp/MainActivity.java
@@ -1,13 +1,73 @@
 package com.example.feedapp;
 
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mFireAuth;
+    private Toolbar mToolbar;
+    private TextInputLayout memail,mpassword;
+    private Button mLogin;
+    private ProgressDialog mLoginProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email=memail.getEditText().getText().toString();
+                String pass=mpassword.getEditText().getText().toString();
+
+                LoginWithEmail(email,pass);
+            }
+        });
+    }
+
+
+    public void init()
+    {
+        memail=(TextInputLayout) findViewById(R.id.textInputLayout);
+        mpassword=(TextInputLayout) findViewById(R.id.textInputLayout2);
+        mLogin=(Button) findViewById(R.id.login_bt);
+        mLoginProgress=new ProgressDialog(MainActivity.this);
+        mToolbar=(Toolbar)findViewById(R.id.login_toolbar);
+        mFireAuth=FirebaseAuth.getInstance();
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Login");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void LoginWithEmail(String email, String pass)
+    {
+        mFireAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful())
+                    {
+
+                    }
+                    else {
+
+                    }
+            }
+        });
     }
 }
